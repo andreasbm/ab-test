f77d45e75953e1a0d130d437dab16e01030bb2e7
andreasbm <andmehlsen@gmail.com>
2019-04-20 17:20:19 +0200

    Added dimensions.

--- src/demo/main.ts
+++ src/demo/main.ts
@@ -7,16 +7,22 @@ import { debounce } from "../lib/util/debounce";
 
 declare global {
 	interface Window {
-		dataLayer: Array<unknown>
+		dataLayer: Array<unknown>;
+		ga: any;
 	}
 }
+
+const gaEvents = ["header.title.text", "header.text.simple-word", "header.text.long", "theme-color", "cta", "element", "element.headline"];
 
 const experiment = new Experiment();
 setExperiment(experiment);
 experiment.addEventListener("update", (e: CustomEvent<Tests>) => {
 	debounce(() => {
 		console.log("Update analytics tool", e.detail);
-		window.dataLayer.push(e.detail);
+		//window.dataLayer.push(e.detail);
+		for (const [key, value] of Object.entries(e.detail)) {
+			window.ga("set", `dimension${gaEvents.indexOf(key) + 1}`, value);
+		}
 
 	}, {ms: 500, id: "commit"})
 });
